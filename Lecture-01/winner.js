5f731ab1a447679c1790532016af124cb59423d8
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2022-11-03 21:15:01 +0530

    Winner between Dolphins and Koalas

---
+++ Lecture-01/winner.js
@@ -0,0 +1,15 @@
+const totalScoreDolphins = (96 + 108 + 89) / 3;
+const totalScoreKoalas = (88 + 91 + 110) / 3;
+
+if(totalScoreDolphins > totalScoreKoalas) {
+    console.log("Dolphins win the trophy 🏆");
+}
+else if(totalScoreKoalas > totalScoreDolphins) {
+    console.log("Koalas win the trophy 🏆");
+}
+else if(totalScoreDolphins === totalScoreKoalas && totalScoreDolphins >= 100 && totalScoreKoalas >= 100){
+    console.log("Both wins the trophy 🏆");
+}
+else {
+    console.log("No one wins the trophy 🏆");
+}
